4a9ac7839c41b2942c0552dc9bdf07f194818c01
Keks <keks@htmlacademy.ru>
2022-07-20 13:11:26 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/main.js
+++ 9/js/main.js
@@ -1,11 +1,9 @@
 import { miniaturesArray} from './data.js';
 import { createMiniImageList } from './creat-miniatures.js';
-//import { viewBigPicture } from './big-picture.js';
-import './big-picture.js';
+import { viewBigPicture } from './big-picture.js';
 import { loadYourPicture } from './form.js';
 import { controlScaleImage } from './control-scale-image.js';
 import { addEffectsImage } from './filter-image.js';
-import { viewBigPicture } from './big-picture.js';
 createMiniImageList(miniaturesArray);
 viewBigPicture();
 loadYourPicture();
